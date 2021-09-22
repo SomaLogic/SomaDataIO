3a434ca0d7abc4585d37ffcd98eea8bd21fd87e9
Stu Field <stu.g.field@gmail.com>
2021-09-22 15:33:46 -0600

    Updates to README.Rmd resulting from recent code improvements
    - closes SomaLogic/SomaDataIO#7

--- R/0-declare-global-variables.R
+++ R/0-declare-global-variables.R
@@ -1,10 +1,11 @@
-##################################
+# --------------------------- #
 # Declaring Global Variables:
 # This is mostly for passing R CMD checks
 # global variables that come from other dependant
 # packages, or objects in the data/ directory
 # Reference: https://github.com/tidyverse/magrittr/issues/29
-##################################
+# ---------------------------------------------------------- #
+if ( getRversion() >= "2.15.1" )
 utils::globalVariables(
   c(".",
     "AptName",
@@ -15,7 +16,6 @@ utils::globalVariables(
     "prefix",
     "rn",
     "SeqId",
-    "seqid",
-    "value"
+    "seqid"
   )
 )
