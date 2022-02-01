d93a643217c27a65955abb5598242a9f6fc4cdbe
Stu Field <stu.g.field@gmail.com>
2022-01-31 20:52:06 -0700

    Update package Startup Message
    - new legal blurb for startup message
      in 'zzz.R'

--- R/zzz.R
+++ R/zzz.R
@@ -52,28 +52,25 @@ create_legal <- function() {
 
       Copyright \u00A9 2022 SomaLogic Operating Co., Inc.
 
-      Permission is hereby granted, free of charge, to any person obtaining
-      a copy of the SomaDataIO software and associated documentation files
-      (the \"Software\"), to deal in the Software without restriction,
-      including without limitation the rights to use, copy, modify, merge,
-      publish, distribute, sublicense, and/or sell copies of the Software,
-      and to permit persons to whom the Software is furnished to do so,
-      subject to the following conditions outlined below. Further,
-      SomaDataIO and SomaLogic are trademarks owned by SomaLogic Operating Co., Inc.
-      No license is hereby granted to these trademarks other than for purposes
-      of identifying the origin or source of the Software.
-
-      The above copyright notice and this permission notice shall be
-      included in all copies or substantial portions of the Software.
+      The `SomaDataIO` package is licensed under the MIT license 
+      (`LICENSE.md`) and is intended solely for research use
+      only (\"RUO\") purposes. The code contained herein may *not*
+      be used for diagnostic, clinical, therapeutic, or other
+      commercial purposes. Further, \"SomaDataIO\" and \"SomaLogic\"
+      are trademarks owned by SomaLogic Operating Co., Inc. No license
+      is hereby granted to these trademarks other than for purposes
+      of identifying the origin or source of the Software. The above
+      copyright notice and this permission notice shall be included
+      in all copies or substantial portions of the Software.
 
       THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,
       EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
       MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDER(S) BE LIABLE FOR
-      ANY CLAIM, DAMAGES, WHETHER DIRECT OR INDIRECT, OR OTHER LIABILITY,
+      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDER(S) BE LIABLE FOR ANY
+      CLAIM, DAMAGES, WHETHER DIRECT OR INDIRECT, OR OTHER LIABILITY,
       WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-      IN THE SOFTWARE."
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+      DEALINGS IN THE SOFTWARE."
     ),
     indent = 0, prefix = "   ", initial = "   "
   )
