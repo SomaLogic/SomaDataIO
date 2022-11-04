96a67298228cb9e4683bd86efec72492ca0f9559
Stu Field <stu.g.field@gmail.com>
2022-11-04 10:32:25 -0600

    Update GitHub Actions to new `r-lib` upgrades
    - update all GitHub Action workflow *.yaml files
    - fixes SomaLogic/SomaDataIO#26

--- R/0-declare-global-variables.R
+++ R/0-declare-global-variables.R
@@ -2,7 +2,7 @@
 # Declaring Global Variables:
 # This is mostly for passing R CMD checks
 # global variables that come from other dependant
-# packages, or objects in the data/ directory
+# packages, or objects in the 'data/' directory
 # Reference: https://github.com/tidyverse/magrittr/issues/29
 # ---------------------------------------------------------- #
 if ( getRversion() >= "2.15.1" )
