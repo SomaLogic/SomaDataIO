b4a3b4fa1bbadf902fb62ff1486612dc485237c7
Stu Field <stu.g.field@gmail.com>
2023-01-11 14:14:08 -0700

    Use 0L for length testing in spell check

--- tests/testthat/test-spelling.R
+++ tests/testthat/test-spelling.R
@@ -4,5 +4,5 @@ test_that("SomaDataIO has no spelling errors", {
   skip_on_covr()
   spell_df <- spelling::spell_check_package(ifelse(is_testing(), "../..", "."))
   spell_df <- dplyr::filter(spell_df, !grepl("Col.Meta", found))  # rm Col.Meta.Rd
-  expect_length(spell_df$word, 0)
+  expect_length(spell_df$word, 0L)
 })
