28d4ef74e6ba44f21dbe8ef02479ae29594eede8
Stu Field <stu.g.field@gmail.com>
2023-01-22 13:17:47 -0700

    Tweak spell checking in unit tests
    - tweak the skip of `Col.Meta.Rd` file
    - add skips for CRAN and CI
    - tweak the context message

--- tests/testthat/test-spelling.R
+++ tests/testthat/test-spelling.R
@@ -1,8 +1,10 @@
 
-test_that("SomaDataIO has no spelling errors", {
+test_that("the `SomaDataIO` package has no spelling errors", {
+  skip_on_cran()
+  skip_on_ci()
   skip_on_check()
   skip_on_covr()
   spell_df <- spelling::spell_check_package(ifelse(is_testing(), "../..", "."))
-  spell_df <- dplyr::filter(spell_df, !grepl("Col.Meta", found))  # rm Col.Meta.Rd
+  spell_df <- dplyr::filter(spell_df, !grepl("Col[.]Meta[.]Rd", found)) # rm Col.Meta.Rd
   expect_length(spell_df$word, 0L)
 })
