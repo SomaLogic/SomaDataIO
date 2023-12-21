1f084eb4c1d99560b26f1b6df4b1a8c350098bfb
Stu Field <stu.g.field@gmail.com>
2023-12-21 11:06:12 -0700

    clean up README links
    - no longer depends on desc pkg

--- inst/check-pkg-versions.R
+++ inst/check-pkg-versions.R
@@ -13,7 +13,30 @@
 #   Stu Field
 # ------------------
 
-tbl <- desc::desc("DESCRIPTION")$get_deps()
+get_deps <- function(path = ".") {
+  dcf <- read.dcf(file = file.path(path, "DESCRIPTION"))
+  if ( nrow(dcf) < 1L) {
+    stop("DESCRIPTION file of package is corrupt.", call. = FALSE)
+  }
+  desc <- as.list(dcf[1L, ])[c("Depends", "Imports", "Suggests")]
+  lapply(desc, parse_deps) |>
+    dplyr::bind_rows(.id = "type")
+}
+
+parse_deps <- function(deps) {
+  deps <- trimws(strsplit(deps, ",")[[1L]])
+  deps <- lapply(strsplit(deps, "\\("), trimws)
+  deps <- lapply(deps, sub, pattern = "\\)$", replacement = "")
+  res <- data.frame(stringsAsFactors = FALSE,
+    package = vapply(deps, "[", "", 1L),
+    version = vapply(deps, "[", "", 2L)
+  )
+  res$version <- gsub("\\s+", " ", res$version)
+  res$version[is.na(res$version)] <- "*"
+  res
+}
+
+tbl <- get_deps(".")
 
 tbl$installed_version <- sapply(tbl$package, function(.x) {
   if (.x == "R")
