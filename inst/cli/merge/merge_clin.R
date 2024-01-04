d75438cdd7e524efeba2f4e74d95da19cc3355c2
Stu Field <stu.g.field@gmail.com>
2024-01-04 14:49:25 -0700

    Bump year 2023 -> 2024
    - fixes #68

--- inst/cli/merge/merge_clin.R
+++ inst/cli/merge/merge_clin.R
@@ -12,7 +12,7 @@
 #      to use for the *.adat (`key1`) and *.csv (`key2`) respectively
 #
 # Copyright:
-#   Copyright (c) 2023 SomaLogic Operating Co., Inc.
+#   Copyright (c) 2024 SomaLogic Operating Co., Inc.
 # Author:
 #   Stu Field
 #
