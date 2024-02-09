198b6cc0e045179054f351be5b14ed5f0b947e59
Stu Field <stu.g.field@gmail.com>
2024-02-09 14:35:40 -0700

    Update transform() and scaleAnalytes()
    - scaleAnalytes() now skips missing references

--- R/scaleAnalytes.R
+++ R/scaleAnalytes.R
@@ -1,18 +1,19 @@
 #' Scale/transform Analyte RFUs
 #'
 #' Scale analytes by a scalar reference value with `SeqId` names to
-#' match with the analytes contained in `.data`.
+#' match with the analytes contained in `.data`. Columns without a
+#' corresponding reference value are not modified (with a warning).
 #'
 #' @param .data A `soma_adat` class object ... _must_ be a `soma_adat` for
 #'   downstream methods to properly dispatch.
 #' @param scale_vec A vector of scalars, named by `SeqId`, see [getSeqId()].
 #' @author Stu Field
 #' @examples
-#' adat <- head(example_data, 3L)
+#' adat <- head(example_data, 3L) |> dplyr::select(id, c(16:18))
 #' apts <- getAnalytes(adat)
-#' ref  <- setNames(rep(1, length(apts)), getSeqId(apts))   # ref = 1.0
+#' ref  <- c("2802-68" = 10.0, "1942-70" = 0.1, "9251-29" = 1.0)
 #' new  <- scaleAnalytes(adat, ref)
-#' identical(new, adat)
+#' new
 #' @importFrom tibble enframe deframe
 #' @noRd
 scaleAnalytes <- function(.data, scale_vec) {
@@ -40,10 +41,10 @@ scaleAnalytes <- function(.data, scale_vec) {
   missing <- setdiff(apts, matches$apts)
   extra   <- setdiff(stbl$SeqId, matches$`stbl$SeqId`)
   if ( length(missing) > 0L ) {
-    stop(
-      "Missing scalar value for ", length(missing), " analytes. ",
-      "Cannot continue.\nPlease check the reference scalars, their names, ",
-      "or the annotations file to proceed.",
+    warning(
+      "Missing scalar value for (", length(missing), ") analytes. ",
+      "They will not be transformed.\n",
+      "Please check the reference or its named SeqIds.",
       call. = FALSE
     )
   }
@@ -58,9 +59,10 @@ scaleAnalytes <- function(.data, scale_vec) {
   svec <- deframe(stbl)               # return to named vector
   svec <- svec[matches$`stbl$SeqId`]  # order reference to the adat
 
-  # should now be identical
-  stopifnot(identical(getSeqId(apts), names(svec)))
+  stopifnot(all(names(svec) %in% getSeqId(apts)))
 
   # apply svec scalars by column
-  transform(.data, unname(svec))
+  new <- transform(.data[, matches$apts, drop = FALSE], unname(svec))
+  .data[, matches$apts] <- data.frame(new, row.names = NULL)
+  .data
 }
