d76adc001830d5ee5befc06d35209487bc277db2
Stu Field <stu.g.field@gmail.com>
2024-02-15 18:29:03 -0700

    Add lifting accessor function for Lin's CCC
    - new function getLiftCCC()
    - accesses the lifting correlations between
      SomaScan versions for each analyte
    - fixed small bug with SomaReadr reference
      in params.R
    - closes #88

---
+++ R/getSomaScanLiftCCC.R
@@ -0,0 +1,25 @@
+#' @rdname adat-helpers
+#'
+#' @description
+#' [getSomaScanLiftCCC()] accesses the lifting Concordance Correlation
+#'   Coefficients between various SomaScan versions.
+#'
+#' @inheritParams params
+#' @references Lin, Lawrence I-Kuei. 1989. A Concordance Correlation
+#'   Coefficient to Evaluate Reproducibility. __Biometrics__. 45:255-268.
+#' @return
+#'   \item{[getSomaScanLiftCCC()]}{Returns a tibble of either the
+#'   `serum` or `plasma` CCC between various versions of the SomaScan assay.}
+#' @examples
+#'
+#' # plasma (default)
+#' getSomaScanLiftCCC()
+#'
+#' # serum
+#' getSomaScanLiftCCC("serum")
+#' @export
+getSomaScanLiftCCC <- function(matrix = c("plasma", "serum")) {
+  matrix <- match.arg(matrix)
+  dplyr::select(lift_master, SeqId,
+                dplyr::starts_with(matrix) & dplyr::ends_with("ccc"))
+}
