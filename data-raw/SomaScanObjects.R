5d92ba26dfd0f7a30412e272fce14f5e4dd3f1fc
scheidec <calebjscheidel@gmail.com>
2025-04-25 15:04:54 -0400

    Add sample annotation merging guidance
    - added `ex_clin_data` object with
      additional sample annotation fields
      `smoking_status` and `alcohol_use` to
      demonstrate merging to a `soma_adat`
      object
    - updated README and loading and wranging
      vignette article with section including
      code to join this object to the
      example_data adat

--- data-raw/SomaScanObjects.R
+++ data-raw/SomaScanObjects.R
@@ -5,12 +5,28 @@ data <- example_data
 x <- ex_analytes
 y <- ex_anno_tbl
 z <- ex_target_names
+zz <- ex_clin_data
 
 # 'new'
 example_data <- read_adat("example_data.adat")  # download via wget
 ex_analytes  <- getAnalytes(example_data)
 ex_anno_tbl  <- getAnalyteInfo(example_data)
 ex_target_names <- getTargetNames(ex_anno_tbl)
+
+withr::with_seed(123, {
+  ex_clin_data <- example_data |>
+    dplyr::mutate(
+      smoking_status = sample(c("Current", "Past", "Never"),
+                              size = 192, replace = TRUE),
+      alcohol_use    = sample(c("Yes", "No"),
+                              size = 192, replace = TRUE)
+    ) |>
+    dplyr::mutate(smoking_status = ifelse(is.na(Age), NA, smoking_status),
+                  alcohol_use    = ifelse(is.na(Age), NA, alcohol_use)) |>
+    select(SampleId, smoking_status, alcohol_use) |>
+    as_tibble()
+})
+
 
 # 'save only if necessary'
 if ( !isTRUE(all.equal(data, example_data)) ) {
@@ -20,6 +36,7 @@ if ( !isTRUE(all.equal(data, example_data)) ) {
 # 'save only if necessary'
 if ( !all(isTRUE(all.equal(x, ex_analytes)),
           isTRUE(all.equal(y, ex_anno_tbl)),
-          isTRUE(all.equal(z, ex_target_names))) ) {
-  save(ex_analytes, ex_anno_tbl, ex_target_names, file = "data/data_objects.rda", compress = "xz")
+          isTRUE(all.equal(z, ex_target_names)),
+          isTRUE(all.equal(zz, ex_clin_data))) ) {
+  save(ex_analytes, ex_anno_tbl, ex_target_names, ex_clin_data, file = "data/data_objects.rda", compress = "xz")
 }
